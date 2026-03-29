d662b505d3f60775d4fa9c7d82ed6a1d5826a84f
Shanmugapriya-developer <priya.pydev@gmail.com>
2026-03-29 10:56:29 +0530

    Update api.js

--- ad frontend/ad_frontend/src/services/api.js
+++ ad frontend/ad_frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API = 'http://localhost:5000/api';
+export const API = "https://ad-management-sztd.onrender.com";
 
 const api = axios.create({
   baseURL: API,
